922ce473845c69395c73d9ba9210ec57c87277ad
lee-dias <leedias.dev@gmail.com>
2024-02-22 21:03:36 +0000

    Add ManyStrings com mais variaveis e com writeline atualizado

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -11,8 +11,19 @@ namespace ManyStrings
             string b = "D:\\LP1\\Joao";
             string c = "Bye\\World \u269D";
             string d = "Hello World!";
-            string e = "varios simbolos \u271E \u262d \u26A1";           
-            Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e);
+            string e = "simbolos \u271E \u262d \u26A1";           
+            Console.WriteLine(a + " | " + b + " | " + c + " | " + d + " | " + e);
+            ///Mais variaveis do tipo String
+            string f = "Hello " + 2 + " Times";
+            string g = "ele disse" + d;
+            string h = "look " + "at " + "me";
+            string i = "um simbolo " + a + " muitos mais " + e;
+            f += " Bye 12 times";
+            string j = $"{a} mais {e} é igual a {a+e}";
+            string k = $"uma pasta {b}";
+            Console.WriteLine(f + " | " + g + " | " + h + " | " + i + " | " + j + " | " + k);
+
+
         }
     }
 }
