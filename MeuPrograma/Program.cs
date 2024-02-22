90052b64595cf1c7142ceb50b5aac5a57be1d54f
lee-dias <leedias.dev@gmail.com>
2024-02-22 20:05:04 +0000

    Add bye world

---
+++ MeuPrograma/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeuPrograma
+{
+    ///Programa a correr
+    class Program
+    {
+        ///Escrever Hello World e Bye World
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye World!");
+        }
+    }
+}
