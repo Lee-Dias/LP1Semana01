c28bf07dd6c13479051ab53a8aee68f63aca6e2f
lee-dias <leedias.dev@gmail.com>
2024-02-22 20:20:42 +0000

    Add Variaveis Reais

--- VariousType/Program.cs
+++ VariousType/Program.cs
@@ -7,14 +7,18 @@ namespace VariousType
         static void Main(string[] args)
         {
             ///Variaveis inteiras
-            sbyte a = 5;
-            int b = 10;
-            ushort c = 15;
-            Console.WriteLine(a + " " + b + " " + c);
+            sbyte int_byte = 5;
+            int inteiro = 10;
+            ushort int_pequeno = 15;
+            Console.WriteLine(int_byte + " " + inteiro + " " + int_pequeno);
             ///Variaveis char
             char coracao = '\u2661';
             char Seta_direita = '\u2192';
             Console.WriteLine(coracao + " " + Seta_direita);
+            ///Variaveis reais
+            float fl = 3.1415F;
+            decimal De = 2.14M;
+            Console.WriteLine(fl + " " + De);
 
         }
     }
