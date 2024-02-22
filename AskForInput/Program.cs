e8a581f4a825c817107fb83c032413b2c9f5796c
lee-dias <leedias.dev@gmail.com>
2024-02-22 21:20:26 +0000

    Add AskForInput a pedir ao utilizador um numero inteiro e a converter a string para inteiro

---
+++ AskForInput/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AskForInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um numero inteiro");
+            string str = Console.ReadLine();
+            int i = int.Parse(str);
+
+        }
+    }
+}
