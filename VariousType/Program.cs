c8204b2831137ecc47e73af76e7ef03a045579a2
lee-dias <leedias.dev@gmail.com>
2024-02-22 20:05:47 +0000

    Add Variaveis Inteiras

---
+++ VariousType/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VariousType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Variaveis inteiras
+            sbyte a = 5;
+            int b = 10;
+            ushort c = 15;
+            Console.WriteLine(a + " " + b + " " + c);
+        }
+    }
+}
