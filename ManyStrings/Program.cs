acb90e0a0626d38c0df10f19c564b0f6ee38d1ea
lee-dias <leedias.dev@gmail.com>
2024-02-22 20:50:13 +0000

    Add ManyStrings com varios caracteres de escape e Unicode

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Variaveis do tipo String
+            string a = "\u2606";
+            string b = "D:\\LP1\\Joao";
+            string c = "Bye\\World \u269D";
+            string d = "Hello World!";
+            string e = "varios simbolos \u271E \u262d \u26A1";           
+            Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e);
+        }
+    }
+}
