acf95bf988a7a5458542efe9f9002733aef17f67
lee-dias <leedias.dev@gmail.com>
2024-02-23 13:39:51 +0000

    add CentraPelaVirgula pede os numeros

---
+++ CentraPelaVirgula/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace CentraPelaVirgula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pedir numeros
+            Console.WriteLine("Introduza 4 numeros reais (um de cada vez)");
+            string var1 = Console.ReadLine();
+            string var2 = Console.ReadLine();
+            string var3 = Console.ReadLine();
+            string var4 = Console.ReadLine();
+            //converter numeros
+            
+            //mostrar numeros
+            Console.WriteLine($" {var1:F2} ");
+        }
+    }
+}
