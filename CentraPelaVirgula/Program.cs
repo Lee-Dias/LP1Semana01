2dcdc5d8f00c93cbbb3e76fe63b7f30dbd0bba3e
lee-dias <leedias.dev@gmail.com>
2024-02-23 14:05:19 +0000

    atualizado CentraPelaVirgula pede os numeros e converte para numeros reais e mostra os alinhados a direita

--- CentraPelaVirgula/Program.cs
+++ CentraPelaVirgula/Program.cs
@@ -7,6 +7,8 @@ namespace CentraPelaVirgula
     {
         static void Main(string[] args)
         {
+            CultureInfo.DefaultThreadCurrentCulture =
+            CultureInfo.InvariantCulture;
             //pedir numeros
             Console.WriteLine("Introduza 4 numeros reais (um de cada vez)");
             string var1 = Console.ReadLine();
@@ -14,9 +16,15 @@ namespace CentraPelaVirgula
             string var3 = Console.ReadLine();
             string var4 = Console.ReadLine();
             //converter numeros
-            
+            float real1 = float.Parse(var1);
+            float real2 = float.Parse(var2);
+            float real3 = float.Parse(var3);
+            float real4 = float.Parse(var4);
             //mostrar numeros
-            Console.WriteLine($" {var1:F2} ");
+            Console.WriteLine(string.Format($" {real1,8:F2} "));
+            Console.WriteLine(string.Format($" {real2,8:F2} "));
+            Console.WriteLine(string.Format($" {real3,8:F2} "));
+            Console.WriteLine(string.Format($" {real4,8:F2} "));
         }
     }
 }
